c182c58dae735e1ef25df80debb4ac178e47780f
yuanchoi <yuanchoi0912@gmail.com>
2022-11-03 17:33:56 +0900

    first commit

--- app/src/main/java/com/teamfive/fridge/HomeActivity.java
+++ app/src/main/java/com/teamfive/fridge/HomeActivity.java
@@ -1,28 +1,4 @@
 package com.teamfive.fridge;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class HomeActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
-
-        //뒤로가기버튼
-        ImageView person = (ImageView) findViewById(R.id.person);
-        person.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+public class HomeActivity {
 }
-
