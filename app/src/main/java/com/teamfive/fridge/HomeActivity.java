7bbfdb571885b7d00027d9bd2ec067fb2c22172c
eunjine <lky2282@abc.com>
2022-11-04 11:15:04 +0900

    first commit

--- app/src/main/java/com/teamfive/fridge/HomeActivity.java
+++ app/src/main/java/com/teamfive/fridge/HomeActivity.java
@@ -1,4 +1,36 @@
 package com.teamfive.fridge;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+        ImageView person = (ImageView) findViewById(R.id.person);
+        person.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView cook = (ImageView) findViewById(R.id.cook);
+        cook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CookActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/teamfive/fridge/SignInActivity.java
+++ app/src/main/java/com/teamfive/fridge/SignInActivity.java
@@ -23,59 +23,5 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        findViewById(R.id.signinButton).setOnClickListener(onClickListener);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            currentUser.reload();
-        }
-    }
-
-
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.signinButton:
-                    signIn();
-
-                    break;
-            }
-        }
-
-    };
-
-    private void signIn() {
-        String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
-        String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
-
-        String nickname = ((EditText)findViewById(R.id.nicknameEditText)).getText().toString();
-
-
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-
-                        }
-                    }
-                });
     }
 }
