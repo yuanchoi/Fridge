52123c94312fc85400b778f3f56065790ab8e98d
yuanchoi <yuanchoi0912@gmail.com>
2022-12-02 22:42:43 +0900

    은진이꺼 수정+추가

--- app/src/main/java/com/teamfive/fridge/MainActivity.java
+++ app/src/main/java/com/teamfive/fridge/MainActivity.java
@@ -1,17 +1,33 @@
 package com.teamfive.fridge;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+
+    private void startSignUpActivity() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
